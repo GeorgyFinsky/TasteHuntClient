2e12c0e5c51825511563a5042fa23b9eef35bff8
GeorgyFinsky <devgeorgyfinsky@gmail.com>
2023-03-16 18:52:05 +0300

    Setup Tabbar

--- TasteHuntClient/Controllers/TabbarController/TabbarController.swift
+++ TasteHuntClient/Controllers/TabbarController/TabbarController.swift
@@ -8,5 +8,37 @@
 import UIKit
 
 final class BaseTabbarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        configurateTabBar()
+        self.tabBar.backgroundColor = .darkGray.withAlphaComponent(0.6)
+        self.tabBar.tintColor = .purple
+        self.tabBar.layer.opacity = 0.6
+    }
+    
+    private func configurateTabBar() {
+        let visitsVC = VisitsController()
+        let cafesVC = CafesController()
+        let settingsVC = SettingsController()
+        
+        self.viewControllers = [visitsVC, cafesVC, settingsVC]
+        visitsVC.tabBarItem = UITabBarItem(
+            title: "Visits",
+            image: UIImage(systemName: "map"),
+            tag: 0
+        )
+        cafesVC.tabBarItem = UITabBarItem(
+            title: "Cafes",
+            image: UIImage(systemName: "cup.and.saucer"),
+            tag: 1
+        )
+        settingsVC.tabBarItem = UITabBarItem(
+            title: "Settings",
+            image: UIImage(systemName: "gear"),
+            tag: 2
+        )
+    }
+    
 }
