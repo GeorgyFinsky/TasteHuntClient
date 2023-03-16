a91e896944a0a8882b039e71e7069fdadde4b8cc
GeorgyFinsky <devgeorgyfinsky@gmail.com>
2023-03-16 18:55:21 +0300

    repository upd

---
+++ TasteHuntClient/Controllers/CafeController/CafeControllers.swift
@@ -0,0 +1,12 @@
+//
+//  CafeControllers.swift
+//  TasteHuntClient
+//
+//  Created by Georgy Finsky on 16.03.23.
+//
+
+import UIKit
+
+final class CafeController: BaseController {
+    
+}
--- TasteHuntClient/Helpers/Environment.swift
+++ TasteHuntClient/Helpers/Environment.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 struct Environment {
-    // Environment.sceneDelegate?.setTabbarAsInitial()
     static var sceneDelegate: SceneDelegate? {
         let scene = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate
         return scene
