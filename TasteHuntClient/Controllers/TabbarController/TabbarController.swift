9d0a137e19ad2820d20f4323c12fbbea2ebb6980
GeorgyFinsky <devgeorgyfinsky@gmail.com>
2023-03-16 18:41:01 +0300

    repository upd

---
+++ TasteHuntClient/Controllers/TabbarController/TabbarController.swift
@@ -0,0 +1,12 @@
+//
+//  TabbarController.swift
+//  TasteHuntClient
+//
+//  Created by Georgy Finsky on 16.03.23.
+//
+
+import UIKit
+
+final class BaseTabbarController: UITabBarController {
+        
+}
