ed61da30f02ed35be3739b653ca8a6ad7038902c
GeorgyFinsky <devgeorgyfinsky@gmail.com>
2023-03-16 11:32:24 +0300

    create isUsernameExist and registerGuest functions

--- TasteHuntClient/Network/TasteHuntProvider.swift
+++ TasteHuntClient/Network/TasteHuntProvider.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import Moya
+
+typealias ArrayResponce<T: Decodable> = (([T]) -> Void)
+typealias ObjectResponce<T: Decodable> = ((T) -> Void)
+typealias Error = ((String) -> Void)
+
+final class TasteHuntProvider {
+    private let provider = MoyaProvider<TasteHuntAPI>(plugins: [NetworkLoggerPlugin()])
+    
+    func isUsernameExist(username: String, success: @escaping ObjectResponce<NameExistingModel>, failure: @escaping Error) {
+        provider.request(.isUsernameExist(username: username)) { result in
+            switch result {
+                case .success(let responce):
+                    guard let result = try? JSONDecoder().decode(NameExistingModel.self, from: responce.data) else { return }
+                    success(result)
+                case .failure(let error):
+                    failure(error.localizedDescription)
+            }
+        }
+    }
+    
+    func registerGuest(id: UUID, username: String, password: String, profileImageURL: String, kitchens: String, visits: String, success: @escaping ObjectResponce<GuestModel>, failure: @escaping Error) {
+        provider.request(.registrateGuest(
+            id: id,
+            username: username,
+            password: password,
+            profileImageURL: profileImageURL,
+            kitchens: kitchens,
+            visits: visits)
+        ) { result in
+        case .success(let responce):
+            guard let result = try? JSONDecoder().decode(NameExistingModel.self, from: responce.data) else { return }
+            success(result)
+        case .failure(let error):
+            failure(error.localizedDescription)
+        }
+    }
+    
+    
+    
+}
