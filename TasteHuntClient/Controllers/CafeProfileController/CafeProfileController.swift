dfa4b1c873219de5c18fb03fc4daa762afba7e72
GeorgyFinsky <devgeorgyfinsky@gmail.com>
2023-03-23 00:14:52 +0300

    update UI

--- TasteHuntClient/Controllers/CafeProfileController/CafeProfileController.swift
+++ TasteHuntClient/Controllers/CafeProfileController/CafeProfileController.swift
@@ -14,8 +14,39 @@ final class CafeProfileController: BaseController {
     
     // MARK: -
     // MARK: UIObjects
+    private lazy var scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+        return scroll
+    }()
+    
+    private lazy var scrollViewContentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .clear
+        return view
+    }()
+    
+    private lazy var backButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        button.imageView?.contentMode = .scaleAspectFit
+        button.setImage(
+            UIImage(systemName: "chevron.backward"),
+            for: .normal
+        )
+        button.addTarget(
+            self,
+            action: #selector(backButtonDidTap(sender:)),
+            for: .touchUpInside
+        )
+        return button
+    }()
+    
     private lazy var profileImageView: UIImageView = {
-       let imageView = UIImageView()
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        imageView.layer.masksToBounds = true
+        imageView.layer.cornerRadius = 100
+        imageView.tintColor = .purple
         return imageView
     }()
     
@@ -35,18 +66,32 @@ final class CafeProfileController: BaseController {
         return label
     }()
     
-    private lazy var workTimeLabel: UILabel = {
-        let label = UILabel()
-        label.numberOfLines = 0
-        label.textColor = .lightGray
-        label.textAlignment = .left
-        label.font = .systemFont(ofSize: 20)
-        return label
+    private lazy var buttonsStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.spacing = 16
+        stack.distribution = .fillEqually
+        return stack
     }()
     
     private lazy var stack: UIStackView = {
         let stack = UIStackView()
         stack.axis = .horizontal
+        stack.distribution = .fillEqually
+        return stack
+    }()
+    
+    private lazy var daysTitleStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 16
+        stack.distribution = .fillEqually
+        return stack
+    }()
+    
+    private lazy var workTimeStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
         stack.spacing = 16
         stack.distribution = .fillEqually
         return stack
@@ -82,13 +127,16 @@ final class CafeProfileController: BaseController {
     
     private lazy var mapView: GMSMapView = {
         let map = GMSMapView()
-        map.layer.cornerRadius = 8
+        map.layer.cornerRadius = 10
         return map
     }()
     
     // MARK: -
     // MARK: Propertes
     private(set) var cafe: CafeInAppModel?
+    private let workDays = WorkTimeLabelType.allCases
+    private var workDaysLabels = [UILabel]()
+    private var workTimeLabels = [UILabel]()
     
     // MARK: -
     // MARK: Lifecircle
@@ -98,11 +146,7 @@ final class CafeProfileController: BaseController {
         setupLayout()
         makeConstraints()
         setupData()
-        setupMapCamera(
-            lat: cafe.coordinates.coordinate.latitude,
-            lon: cafe.coordinates.coordinate.longitude,
-            zoom: 8
-        )
+        createMarker(coordinates: cafe.coordinates)
     }
     
     func set(cafe: CafeInAppModel) {
@@ -111,9 +155,9 @@ final class CafeProfileController: BaseController {
     
     private func setupData() {
         guard let cafe else { return }
+        self.profileImageView.sd_setImage(with: URL(string: cafe.profileImageURL))
         self.nameLabel.text = cafe.name
         self.phoneLabel.text = cafe.phone
-        self.workTimeLabel.text = "Monday: \(cafe.mondayWorkTime)\nThesday: \(cafe.thesdayWorkTime)\nWednesday: \(cafe.wednesdayWorkTime)\nThusday: \(cafe.thusdayWorkTime)\nFriday: \(cafe.fridayWorkTime)\nSaturday \(cafe.saturdayWorkTime)\nSunday: \(cafe.sundayWorkTime)"
     }
     
 }
@@ -122,19 +166,87 @@ final class CafeProfileController: BaseController {
 // MARK: SetupUI
 extension CafeProfileController {
     
+    private func createLabel() -> UILabel {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textColor = .lightGray
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 20)
+        return label
+    }
+    
     private func setupLayout() {
-        self.contentView.addSubview(nameLabel)
-        self.contentView.addSubview(phoneLabel)
-        self.contentView.addSubview(stack)
-        self.stack.addArrangedSubview(createVisitButton)
-        self.stack.addArrangedSubview(viewMenuButton)
-        self.contentView.addSubview(mapView)
-        self.contentView.addSubview(workTimeLabel)
+        self.contentView.addSubview(scrollView)
+        self.scrollView.addSubview(scrollViewContentView)
+        self.scrollViewContentView.addSubview(profileImageView)
+        self.scrollViewContentView.addSubview(backButton)
+        self.scrollViewContentView.addSubview(nameLabel)
+        self.scrollViewContentView.addSubview(phoneLabel)
+        self.scrollViewContentView.addSubview(buttonsStack)
+        self.scrollViewContentView.addSubview(mapView)
+        self.scrollViewContentView.addSubview(stack)
+        self.buttonsStack.addArrangedSubview(createVisitButton)
+        self.buttonsStack.addArrangedSubview(viewMenuButton)
+        self.stack.addArrangedSubview(daysTitleStack)
+        self.stack.addArrangedSubview(workTimeStack)
+        
+        workDays.forEach { day in
+            let label = createLabel()
+            let workTimeLabel = createLabel()
+            let index = workDaysLabels.count
+            
+            label.text = day.rawValue
+            self.daysTitleStack.addArrangedSubview(label)
+            self.workDaysLabels.append(label)
+            
+            switch index {
+                case 0:
+                    workTimeLabel.text = self.cafe?.mondayWorkTime
+                case 1:
+                    workTimeLabel.text = self.cafe?.thesdayWorkTime
+                case 2:
+                    workTimeLabel.text = self.cafe?.wednesdayWorkTime
+                case 3:
+                    workTimeLabel.text = self.cafe?.thusdayWorkTime
+                case 4:
+                    workTimeLabel.text = self.cafe?.fridayWorkTime
+                case 5:
+                    workTimeLabel.text = self.cafe?.saturdayWorkTime
+                case 6:
+                    workTimeLabel.text = self.cafe?.sundayWorkTime
+                default: break
+            }
+            
+            self.workTimeStack.addArrangedSubview(workTimeLabel)
+            self.workTimeLabels.append(workTimeLabel)
+        }
     }
     
     private func makeConstraints() {
-        nameLabel.snp.makeConstraints { make in
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+            make.width.height.equalTo(self.view)
+        }
+        
+        scrollViewContentView.snp.makeConstraints { make in
+            make.top.bottom.equalTo(scrollView)
+            make.leading.trailing.equalTo(self.view)
+        }
+        
+        backButton.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(16)
+            make.leading.equalToSuperview().inset(16)
+            make.height.width.equalTo(40)
+        }
+        
+        profileImageView.snp.makeConstraints { make in
             make.top.equalToSuperview().offset(40)
+            make.centerX.equalToSuperview()
+            make.height.width.equalTo(200)
+        }
+        
+        nameLabel.snp.makeConstraints { make in
+            make.top.equalTo(profileImageView.snp.bottom).offset(30)
             make.centerX.equalToSuperview()
         }
         
@@ -151,30 +263,37 @@ extension CafeProfileController {
             make.height.equalTo(40)
         }
         
-        stack.snp.makeConstraints { make in
+        buttonsStack.snp.makeConstraints { make in
             make.top.equalTo(phoneLabel.snp.bottom).offset(20)
             make.leading.equalToSuperview().inset(16)
             make.trailing.equalToSuperview().inset(16)
         }
         
         mapView.snp.makeConstraints { make in
-            make.top.equalTo(stack.snp.bottom).offset(20)
-            make.height.equalTo(200)
+            make.top.equalTo(buttonsStack.snp.bottom).offset(20)
+            make.height.equalTo(300)
             make.leading.trailing.equalToSuperview().inset(16)
             make.centerX.equalToSuperview()
         }
         
-        workTimeLabel.snp.makeConstraints { make in
+        stack.snp.makeConstraints { make in
             make.top.equalTo(mapView.snp.bottom).offset(20)
-            make.leading.equalToSuperview().inset(16)
-            make.trailing.equalToSuperview().inset(16)
+            make.leading.leading.equalToSuperview().inset(20)
+            make.bottom.equalToSuperview()
         }
+        
     }
     
-    private func setupMapCamera(lat: Double, lon: Double, zoom: Float) {
-        let camera = GMSCameraPosition.camera(withLatitude: lat, longitude: lon, zoom: zoom)
+    private func createMarker(coordinates: CLLocation) {
+        let marker = GMSMarker(position: CLLocationCoordinate2D(
+            latitude: coordinates.coordinate.latitude,
+            longitude: coordinates.coordinate.longitude
+        ))
+        marker.icon = GMSMarker.markerImage(with: UIColor.purple)
+        marker.map = mapView
         
-        mapView.animate(to: camera)
+        mapView.animate(toLocation: marker.position)
+        mapView.animate(toZoom: 15)
     }
     
 }
@@ -182,6 +301,10 @@ extension CafeProfileController {
 // MARK: -
 // MARK: Buttons Action
 extension CafeProfileController {
+    
+    @objc func backButtonDidTap(sender: UIBarButtonItem) {
+        self.navigationController?.popViewController(animated:true)
+    }
     
     @objc private func createVisitButtonDidTap() {
         guard let cafe else { return }
